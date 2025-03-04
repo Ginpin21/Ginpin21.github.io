c0c6a0dd00de1a193ef03b1f7c9df90bea64dd0c
Ginpin21 <iftekharahmed2003@gmail.com>
2025-03-04 17:29:08 +0400

    Updated stuff

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import ContactSection from "./components/sections/ContactSection";
 import Nav from "./components/Nav";
 const App = () => {
   return (
-    <main className="snap-y overflow-x-hidden overflow-y-scroll h-[100dvh] flex-grow z-0 snap-mandatory scroll-smooth">
+    <main className="snap-y overflow-x-hidden overflow-y-scroll h-[100dvh] flex-grow z-0 snap-mandatory scroll-smooth container mx-auto">
       <Nav />
       <HomeSection />
       <AboutSection />
--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -6,36 +6,33 @@ import NodeLogo from "../../assets/skills/Node.png";
 import VueLogo from "../../assets/skills/Vue.png";
 import NuxtLogo from "../../assets/skills/Nuxt.png";
 import TypescriptLogo from "../../assets/skills/Typescript.png";
+import JavascriptLogo from "../../assets/skills/Javascript.png";
 import ExpressLogo from "../../assets/skills/Express.png";
+import AdonisLogo from "../../assets/skills/Adonis.png";
 import SupabaseLogo from "../../assets/skills/Supabase.png";
+import PythonLogo from "../../assets/skills/Python.png";
+import FastapiLogo from "../../assets/skills/Fastapi.png";
 const SkillsSection = () => {
   const skillLogos = [
+    TypescriptLogo,
+    JavascriptLogo,
     ReactLogo,
     NextLogo,
-    TailwindLogo,
     VueLogo,
     NuxtLogo,
-
+    TailwindLogo,
     NodeLogo,
     ExpressLogo,
-
-    TypescriptLogo,
+    AdonisLogo,
     SupabaseLogo,
+    PythonLogo,
+    FastapiLogo,
   ];
   return (
     <motion.section
       id="skills"
-      className="font-ubuntu h-full relative p-5 container flex flex-col md:flex-row items-center justify-center gap-20 snap-start snap-always"
+      className="font-ubuntu min-h-full relative p-5 container flex flex-col items-center justify-center gap-20 snap-start snap-always"
     >
-      {/* <a href="#about" className="absolute right-2 top-10 hover:text-blue-800">
-        <CaretCircleUp size={24} weight="duotone" />
-      </a>
-      <a
-        href="#projects"
-        className="absolute right-2 bottom-10 hover:text-blue-800"
-      >
-        <CaretCircleDown size={24} weight="duotone" />
-      </a> */}
       <motion.h1
         className="text-5xl font-bold text-blue-800"
         initial={{ opacity: 0 }}
@@ -44,18 +41,23 @@ const SkillsSection = () => {
       >
         Skills
       </motion.h1>
-      <div className="grid grid-cols-3 items-center gap-5">
+      <div className="grid md:grid-cols-2 lg:grid-cols-5 items-center gap-2 w-full place-items-center">
         {skillLogos.map((logo, index) => (
-          <motion.img
-            loading="lazy"
+          <motion.div
             key={index}
-            src={logo}
-            className="w-36"
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.2 }}
-            transition={{ duration: 1 }}
-          />
+            className="w-36 h-36 rounded-md border-2 border-transparent hover:border-slate-200 p-5 flex flex-col"
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.2 }}
+          >
+            <motion.img
+              loading="lazy"
+              src={logo}
+              className="w-full h-full object-contain"
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+            />
+          </motion.div>
         ))}
       </div>
     </motion.section>
