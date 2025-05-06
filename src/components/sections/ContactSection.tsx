00232aa0b02a5bb16ea8fa1a7c801c801a14e8c3
Ginpin21 <iftekharahmed2003@gmail.com>
2025-05-06 14:05:58 +0400

    fix: Updated font size in footer

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -7,9 +7,9 @@ import { motion } from "framer-motion";
 
 const ContactSection = () => {
   return (
-    <motion.section
+    <section
       id="contact"
-      className="font-ubuntu relative px-5 lg:px-10 pt-10 pb-30 flex flex-col  items-center justify-center md:justify-center gap-10 bg-gradient-to-t from-cyan-900/50"
+      className="font-ubuntu relative px-5 lg:px-10 pt-10 pb-30 flex flex-col  items-center justify-center gap-10 bg-gradient-to-t from-cyan-900/50"
     >
       <motion.h2
         initial={{ y: 50 }}
@@ -22,12 +22,12 @@ const ContactSection = () => {
       <motion.div
         initial={{ y: 50 }}
         whileInView={{ y: 0 }}
-        className="flex flex-col items-start lg:flex-row gap-5 text-2xl text-cyan-200"
+        className="flex flex-col items-center lg:flex-row gap-5 text-xl lg:text-2xl text-cyan-200"
       >
         <a
           target="_blank"
           href="mailto:iftekarahmed2003@gmail.com"
-          className="flex gap-2 items-center hover:text-cyan-700"
+          className="flex gap-2 items-center hover:text-cyan-700 active:text-cyan-700"
         >
           <EnvelopeSimple weight="duotone" size={30} />
           <p>iftekarahmed2003@gmail.com</p>
@@ -35,7 +35,7 @@ const ContactSection = () => {
         <a
           target="_blank"
           href="https://www.linkedin.com/in/iftekharahmed21/"
-          className="flex gap-2 items-center hover:text-cyan-700"
+          className="flex gap-2 items-center hover:text-cyan-700 active:text-cyan-700"
         >
           <LinkedinLogo weight="duotone" />
           <p>Iftakhar Ahmed</p>
@@ -43,16 +43,16 @@ const ContactSection = () => {
         <a
           target="_blank"
           href="https://github.com/Ginpin21"
-          className="flex gap-2 items-center hover:text-cyan-700"
+          className="flex gap-2 items-center hover:text-cyan-700 active:text-cyan-700"
         >
           <GithubLogo weight="duotone" />
           <p>Ginpin21</p>
         </a>
       </motion.div>
-      <motion.h2 initial={{ y: 50 }} whileInView={{ y: 0 }} whileHover={{scale:1.1}}>
-        <a target="_blank" href="/CV.pdf">Want to know more? Get my CV <span className="underline">here</span></a>
+      <motion.h2 initial={{ y: 50 }} whileInView={{ y: 0 }} whileHover={{scale:1.1}} className="text-center">
+        <a target="_blank" href="/CV.pdf">Want to know more? <br className="lg:hidden"/>Get my CV <span className="underline">here</span></a>
       </motion.h2>
-    </motion.section>
+    </section>
   );
 };
 
